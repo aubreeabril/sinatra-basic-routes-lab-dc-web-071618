056625cd535e4264c48738a74df026fba536db13
aubreeabril <aubree.abril@gmail.com>
2018-08-06 10:37:54 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    'My name is Aubree Abril'
+  end
+
+  get '/hometown' do
+    'My hometown is Tucson'
+  end
+
+  get '/favorite-song' do
+    'My favorite song is Grand Canyon'
+  end
 end
